dc1fc7ea0b4b7f6b652af3217325f1faf2f0184b
liqingrui <2636045725@qq.com>
2020-03-23 10:48:07 +0800

    vs code 第一次提交Git

---
+++ src/main/java/com/dada/demo/App.java
@@ -0,0 +1,14 @@
+package com.dada.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( final String[] args)
+    {
+        System.out.println( "Hello World!" );
+        // System.exit(0);
+    }
+}
